856e3f9c613da4305f64448028ea3ba7be2920aa
gabemontero <gmontero@redhat.com>
2025-08-28 10:47:42 -0400

    ODH can now run with serverless/servicemesh

--- subscriptions.sh
+++ subscriptions.sh
@@ -13,36 +13,6 @@ while true; do
 done
 oc wait --for=jsonpath='{.status.phase}'=Succeeded "$AUTHORINO" -n openshift-operators --timeout=300s
 
-oc apply -f ./servicemesh-subscription.yaml
-while true; do
-	oc get csv -o name -n openshift-operators | grep servicemesh > mesh.txt
-	if [ -s mesh.txt ]; then
-		export SERVICEMESH=$(cat mesh.txt)
-		echo "servicemesh csv is ${SERVICEMESH}"
-		break
-	else
-		echo "mesh.txt still emtpy"
-		sleep 5
-	fi
-done
-oc wait --for=jsonpath='{.status.phase}'=Succeeded "$SERVICEMESH" -n openshift-operators --timeout=300s
-
-
-oc apply -f ./serverless-subscription.yaml
-while true; do
-	oc get csv -o name -n openshift-serverless | grep serverless > serverless.txt
-	if [ -s serverless.txt ]; then
-		export SERVERLESS=$(cat serverless.txt)
-		echo "serverless csv is ${SERVERLESS}"
-		break
-	else
-		echo "serverless.txt still empty"
-		sleep 5
-	fi
-done
-oc wait --for=jsonpath='{.status.phase}'=Succeeded "$SERVERLESS" -n openshift-serverless --timeout=300s
-
-
 oc apply -f ./opendatahub-subscription.yaml
 while true; do
 	oc get csv -o name -n openshift-operators | grep opendatahub > hub.txt
