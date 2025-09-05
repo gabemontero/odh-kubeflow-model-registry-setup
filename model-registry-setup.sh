5922f9382438433c28d696d27d40d854bde63c05
gabemontero <gmontero@redhat.com>
2025-09-05 16:51:25 -0400

    adjust model catalog dashboard sync with model catalog REST service for late Aug/early Sept changes

--- model-registry-setup.sh
+++ model-registry-setup.sh
@@ -8,7 +8,6 @@ oc wait --for=condition=available deployment/model-registry-db --timeout=5m
 oc apply -f odh-model-registrires-ns.yaml
 
 oc project odh-model-registries
-oc apply -k ./rhoai-model-catalog-rest -n odh-model-registries
 oc apply -f registry.yaml
 oc wait --for=condition=available modelregistry.modelregistry.opendatahub.io/modelregistry-public --timeout=5m
 
@@ -16,5 +15,11 @@ oc apply -f odh-admins.yaml
 
 # should not be needed long term by model registry / odh-model-controller
 oc set env  deployment/odh-model-controller -n opendatahub MR_SKIP_TLS_VERIFY=true
-oc wait --for=jsonpath='{.status.observedGeneration}'=2 deployment/odh-model-controller -n opendatahub --timeout=300s
+sleep 60
+#oc wait --for=jsonpath='{.status.observedGeneration}'=2 deployment/odh-model-controller -n opendatahub --timeout=300s
+
+oc apply -f default-model-catalog-cm.yaml
+oc apply -f model-catalog-sources-cm.yaml
+
+oc get pods -o name | grep catalog | xargs -l -r oc delete 
 
