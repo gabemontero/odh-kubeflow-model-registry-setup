916d1523c9e3cbed9b12799dee811f1332d7b749
gabemontero <gmontero@redhat.com>
2025-10-03 16:46:19 -0400

    update odh install to account for 2.35 needs and abilities

--- model-registry-setup.sh
+++ model-registry-setup.sh
@@ -5,7 +5,7 @@ oc wait --for=condition=available deployment/model-registry-db --timeout=5m
 
 # https://issues.redhat.com/browse/RHOAIENG-21954 and https://github.com/opendatahub-io/model-registry-operator/pull/216 should
 # eliminate the need for this; will work with RHOAI team to see when a ODH or RHOAI operator has this change
-oc apply -f odh-model-registrires-ns.yaml
+#oc apply -f odh-model-registrires-ns.yaml
 
 oc project odh-model-registries
 oc apply -f registry.yaml
@@ -19,16 +19,14 @@ sleep 60
 
 oc get pods -n opendatahub
 
-# oc apply -f default-model-catalog-cm.yaml
+# ODH 2.35 work around for dashboard mismatch
+oc scale --replicas=0 deployment/opendatahub-operator-controller-manager -n openshift-operators
+sleep 45
 
-oc scale --replicas=0 deployment/model-registry-operator-controller-manager -n opendatahub
-sleep 45
-oc delete cm default-model-catalog -n odh-model-registries
-oc create configmap default-model-catalog --from-file=default-catalog.yaml=./test-yaml-catalog.yaml -n odh-model-registries
-oc apply -f model-catalog-sources-cm.yaml
-oc get pods -o name -n odh-model-registries | grep catalog | xargs -l -r oc delete -n odh-model-registries
-oc scale --replicas=1 deployment/model-registry-operator-controller-manager -n opendatahub
-sleep 45
+oc set image deployment/odh-dashboard model-registry-ui=quay.io/opendatahub/odh-mod-arch-modular-architecture:v2.38.1-odh -n opendatahub
+
+sleep 120
+
 oc get pods -n opendatahub
 oc get pods -n odh-model-registries
 oc get routes -n odh-model-registries
